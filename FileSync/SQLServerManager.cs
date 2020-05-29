a299840cee39a89865215259a49b1efd4919640f
Oswald Toma <oswald.toma@gmail.com>
2020-05-29 17:31:20 +0200

    packages

---
+++ FileSync/SQLServerManager.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSync
+{
+    public static class SQLServerManager
+    {
+        public static void InitializeConnection()
+        {
+            var sqlConnectionString = "";
+            var sqlConnection = new SqlConnection(sqlConnectionString);
+        }
+    }
+}
