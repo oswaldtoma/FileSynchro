ea12fd4521936c4660dc65e7ba4cf9fcd5a91e3b
Oswald Toma <oswald.toma@gmail.com>
2020-05-30 20:41:11 +0200

    Established connection with my database

---
+++ FileSync/File.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSync
+{
+    public class File
+    {
+        public int FileId { get; set; }
+        public string FileName { get; set; }
+        public string SHA1Checksum { get; set; }
+        public DateTime? FileCreationDate { get; set; }
+        public DateTime? FileLastModificationDate { get; set; }
+        public DateTime? FilePlacementDate { get; set; }
+        public string FileType { get; set; }
+        public int FileSize { get; set; }
+        public string FileLocation { get; set; }
+    }
+}
--- FileSync/Program.cs
+++ FileSync/Program.cs
@@ -4,9 +4,17 @@ using System.Linq;
 using System.Net.NetworkInformation;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Entity;
 
 namespace FileSync
 {
+    public class FileSyncContext : DbContext
+    {
+        public FileSyncContext() : base("name=FileSyncDbConnectionStr")
+        { 
+        }
+        public DbSet<File> Files { get; set; }
+    }
     static class Program
     {
         /// <summary>
@@ -17,7 +25,19 @@ namespace FileSync
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+        //    Application.Run(new Form1());
+
+            using (var db = new FileSyncContext())
+            {
+                db.Database.CreateIfNotExists();
+                var file = new File
+                {
+                    FileId = 1,
+                    FileName = "plik testowy"
+                };
+                db.Files.Add(file);
+                db.SaveChanges();
+            }
         }
     }
 }
